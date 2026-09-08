6ff9052bc5fe8be883a32d024b362b9c3ec36f46
Thowseena Safrin <thowseenasafrin@gmail.com>
2026-09-08 20:53:05 +0530

    Time: 10 ms (38.01%), Space: 55.9 MB (17.86%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        int count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                count++;
+                if (count > nums.length / 2) {
+                    return nums[i];
+                }
+            } else {
+                count = 1; 
+            }
+        }
+        return nums[0]; 
+    }
+}
