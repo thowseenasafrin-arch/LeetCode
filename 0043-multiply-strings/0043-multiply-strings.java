28e6ffe6abe3cd1de04b274222b5a24d0cf296bb
Thowseena Safrin <thowseenasafrin@gmail.com>
2026-09-23 09:57:17 +0530

    Time: 3 ms (77.43%), Space: 43.5 MB (63.17%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.java
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -1,21 +1,30 @@
 class Solution {
     public String multiply(String num1, String num2) {
-        if (num1.equals("0") || num2.equals("0")) return "0";
-        int n1 = num1.length();
-        int n2 = num2.length();
-        int[] result = new int[n1 + n2];
-         for (int i = n1 - 1; i >= 0; i--) {
-            for (int j = n2 - 1; j >= 0; j--) {
-                int mul=(num1.charAt(i)-'0')*(num2.charAt(j)-'0');
-                int sum=mul+result[i+j+1];
-                result[i+j+1]=sum%10;
-                result[i+j]+=sum/10;
+        if (num1.equals("0") || num2.equals("0")) {
+            return "0";
+        }
+        
+        int m = num1.length(), n = num2.length();
+        int[] result = new int[m + n];
+        
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                int p1 = i + j, p2 = i + j + 1;
+                int total = mul + result[p2];
+                
+                result[p2] = total % 10;
+                result[p1] += total / 10;
             }
-         }
-         StringBuilder sb=new StringBuilder();
-         int i = 0;
-         while(i<result.length&&result[i]==0)i++;
-         for(;i<result.length;i++)sb.append(result[i]);
-         return sb.toString();
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for (int num : result) {
+            if (!(sb.length() == 0 && num == 0)) {
+                sb.append(num);
+            }
+        }
+        
+        return sb.length() == 0 ? "0" : sb.toString();
     }
 }
